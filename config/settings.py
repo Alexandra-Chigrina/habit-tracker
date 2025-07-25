6fa6b5593c3e2007c077e8c340ae9ac9b35f2338
Sasha <a.chigrina89@gmail.com>
2025-07-25 18:13:20 +0300

    add settings fixes

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "django_celery_beat",
     "corsheaders",
     "drf_yasg",
