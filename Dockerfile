cf596318bcc7e65de9a04e71fa4b9afbf97274f2
Sasha <a.chigrina89@gmail.com>
2025-08-10 02:29:41 +0300

    add docker-compose.yml

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:3.13-slim
+
+WORKDIR /app
+
+
+RUN apt-get update && \
+    apt-get install -y gcc libpq-dev --no-install-recommends && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+COPY pyproject.toml poetry.lock ./
+
+RUN pip install --no-cache-dir poetry
+RUN poetry config virtualenvs.create false \
+    && poetry install --no-root --no-interaction
+
+COPY . .
+
+EXPOSE 8000
+
+CMD ["gunicorn", "config.wsgi:application", "--bind", "0.0.0.0:8000"]
--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -105,6 +104,7 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -118,15 +118,14 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True                # для разработки!
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://django",
+    f"http://{os.getenv('VM_IP')}",
 ]
-
-CORS_ALLOW_ALL_ORIGINS = False
 
 CELERY_TIMEZONE = TIME_ZONE
 CELERY_TASK_TRACK_STARTED = True
